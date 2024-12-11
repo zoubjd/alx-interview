4e36b68ebc98f5c32f014d02f577dadc216ef548
zoubjd <zouhairbajdouri19@gmail.com>
2024-12-11 17:37:16 +0100

    primeGame

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""prime game"""
+
+
+def isPrime(n):
+    """checks if a num is prime"""
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+
+    return True
+
+
+def primeList(num):
+    """returns a list of prime numbers"""
+    return [i for i in range(2, num + 1) if isPrime(i)]
+
+
+def isWinner(x, nums):
+    if x <= 0 or len(nums) == 0 or x > len(nums):
+        return None
+
+    maria = 0
+    ben = 0
+
+    for rounds in range(x):
+        primes = primeList(nums[rounds])
+        if len(primes) % 2 == 1:
+            maria += 1
+        else:
+            ben += 1
+
+    if maria > ben:
+        return "Maria"
+    elif maria < ben:
+        return "Ben"
+    else:
+        return None
---
+++ 0x0A-primegame/README.md
@@ -0,0 +1,2 @@
+the prime game played by ben and maria
+0-prime_game.py
